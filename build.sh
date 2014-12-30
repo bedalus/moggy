e85098a91fdb0ffa341696f4f5735e6062dd9e55
Dave Savage <bedalus@gmail.com>
2014-12-30 18:53:40 +0000

    initial build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+echo -e "Making moto g 4g (peregrine) zImage\n"
+export PATH=$PATH:/opt/toolchain/bin/
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabi-
+
+make peregrine_defconfig
+
+make -j7
+
+# modules
+find ./ -type f -name '*.ko' -exec cp -f {} ../zip/system/lib/modules/ \;
+
+# copy zImage
+cp -f arch/arm/boot/zImage-dtb ../zip/kernel/
+ls -l ../zip/kernel/zImage-dtb
+cd ../zip
+zip -r -9 moggy_vN.zip * > /dev/null
+mv moggy_vN.zip ../
